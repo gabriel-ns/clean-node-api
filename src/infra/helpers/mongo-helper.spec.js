2de20361eae48324d04a1f66474a21820ebc7842
Gabriel Santos <gaabrielns@gmail.com>
2021-03-07 20:24:59 -0300

    break MongoHelper tests into smaller tests

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,12 +1,23 @@
 const MongoHelper = require('./mongo-helper')
 
 describe('Mongo Helper', () => {
-  test('Should reconnect when getDb() is invoked and client is disconnected', async () => {
+  test('Should have a valid db after connected', async () => {
     const sut = new MongoHelper()
     await sut.connect(process.env.MONGO_URL)
     expect(sut.db).toBeTruthy()
+  })
+
+  test('Should have a null db after connected', async () => {
+    const sut = new MongoHelper()
+    await sut.connect(process.env.MONGO_URL)
     await sut.disconnect()
     expect(sut.db).toBeFalsy()
+  })
+
+  test('Should reconnect when getDb() is invoked and client is intentionally disconnected', async () => {
+    const sut = new MongoHelper()
+    await sut.connect(process.env.MONGO_URL)
+    await sut.disconnect()
     await sut.getDb()
     expect(sut.db).toBeTruthy()
   })
