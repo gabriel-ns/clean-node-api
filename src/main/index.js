53e36370437b45d37494035fd7e08d271aef635d
Gabriel Santos <gaabrielns@gmail.com>
2021-03-07 21:35:41 -0300

    ensure x-powered-by header is disabled

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,7 @@
-const express = require('express')
-const app = express()
+const app = require('./config/app')
+
+app.get('/api/gabs', (req, res) => {
+  res.send('gabs')
+})
 
 app.listen(8080, () => console.log('server running'))
