5b3726d0f50fc4615fa7ed187e6d0e509822897a
Gabriel Santos <gaabrielns@gmail.com>
2021-03-08 23:52:22 -0300

    fix MongoHelper connection in main

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,7 @@
 const MongoHelper = require('../infra/helpers/mongo-helper')
 const env = require('./config/env')
 
-const mongoHelper = new MongoHelper()
-
-mongoHelper.connect(env.mongoUrl)
+MongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app')
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
